8989c6b12f37d91c476c421fb80cbda968f929a1
Arthur Vinicius <thuur.vss@gmail.com>
2021-10-31 20:10:08 -0300

    Sessão 3 - Trabalhando com Jobs e Steps

---
+++ primeiro-projeto-springbatch/src/main/java/com/udemy/primeiroprojetospringbatch/PrimeiroProjetoSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeiroprojetospringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroProjetoSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroProjetoSpringbatchApplication.class, args);
+	}
+
+}
